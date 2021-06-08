d147df8a5148cab0349faf7cf8e1935985dd0504
babyaced <dannyr.simpson@gmail.com>
2021-06-07 19:54:27 -0700

    changed positioning of view code and see demo buttons

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,17 +8,15 @@ function ProjectCard({projectTitle, projectRepo, projectTechStack, style, projec
         <article className={styles.projectCard} style={style}>
             <div className={styles.projectCardHeader}>
                 <h2>{projectTitle}</h2>
-                <a href={projectRepo} target="_blank" rel="noreferrer">View Code</a>
             </div>
+            <a href={projectRepo} target="_blank" rel="noreferrer">View Code</a>
+            <a href={projectLink} target="_blank" rel="noreferrer">See Demo</a>
             <ul className={styles.techStack}>
                 {Object.entries(projectTechStack).map(([name, logo]) =>
                     <li key={name}>
                         <img src={logo} alt="" title={name}/>
                     </li>
                 )}
-                <li>
-                    <a href={projectLink} target="_blank" rel="noreferrer">See Demo</a>
-                </li>
             </ul>
         </article>
     )
